e6acc82612747d0e0b09416e27a4965b729808e5
Ali Ekmekci <aliosmanekmekci@gmail.com>
2024-12-16 12:29:07 +0300

    Complete Exercise Number 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function (param1, param2) {
+  let sum = 0;
+  if (param1 < param2 && param1 && param2) {
+    for (let i = param1; i <= param2; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
