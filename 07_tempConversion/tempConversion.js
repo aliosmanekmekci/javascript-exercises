24d5fc54a8aacbd31ca85084daddea8ddc04e5a1
Ali Ekmekci <aliosmanekmekci@gmail.com>
2024-12-16 13:03:43 +0300

    Complete Exercise Number 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (param) {
+  return ((param - 32) * 5) / 9;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (param) {
+  return (param * 9) / 5 + 32;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
