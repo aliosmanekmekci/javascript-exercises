c4ad336748206973db182c0dbd106e30af2200f5
Ali Ekmekci <aliosmanekmekci@gmail.com>
2024-12-16 16:49:03 +0300

    Passed all tests in 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,7 @@
-const leapYears = function (param) {
-  if (typeof param === "number") {
-    if (param % 4 === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  } else return false;
+const leapYears = function (year) {
+return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
 };
-leapYears(1996);
+
 
 // Do not edit below this line
 module.exports = leapYears;
