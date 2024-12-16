1a8af2a2bfbe9ad0e40c89f57071b490503e3e09
Ali Ekmekci <aliosmanekmekci@gmail.com>
2024-12-16 12:47:29 +0300

    Complete exercise number 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (param) {
+  if (typeof param === "number") {
+    if (param % 4 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else return false;
 };
+leapYears(1996);
 
 // Do not edit below this line
 module.exports = leapYears;
