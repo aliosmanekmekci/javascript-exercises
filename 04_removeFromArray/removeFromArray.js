080b43fb7d21a51d4c391b8b6de2f58d7c3623db
Ali Ekmekci <aliosmanekmekci@gmail.com>
2024-12-16 12:17:38 +0300

    Complete Exercise Number 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(param, n) {
+    const index = param.indexOf(n)
+    if (n > -1) {
+        param.splice(index, 1)
+    }
+    return param;
 };
 
 // Do not edit below this line
