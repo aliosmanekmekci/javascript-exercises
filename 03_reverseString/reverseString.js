b90d4529b677425edc51e4724805894e8f126012
Ali Ekmekci <aliosmanekmekci@gmail.com>
2024-12-16 12:10:12 +0300

    Complete exercise number 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (param) {
+  let paramToArr = param;
+  const ans = paramToArr.split("").reverse().join("");
+    return ans;
 };
-
+reverseString("hey");
 // Do not edit below this line
 module.exports = reverseString;
