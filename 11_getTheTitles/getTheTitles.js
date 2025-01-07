de87e166f36b8b47efca242172117c13542cba58
Ali Ekmekci <aliosmanekmekci@gmail.com>
2025-01-07 03:25:22 +0300

    Pass the test 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    let map = books.map(book => book.title);
+    return map;
 };
+
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
+
+const result = getTheTitles(books);
+
+console.log(result);
 
 // Do not edit below this line
 module.exports = getTheTitles;
